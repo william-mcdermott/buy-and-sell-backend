e92eaa191b914e11b6529fd2bb974c30e6ea0744
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-03-19 21:09:05 -0500

    Add route to get listings by user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { addViewToListingRoute } from "./addViewToListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
+import { getUserListingsRoute } from "./getUserListings";
 
 export default [
     getAllListingsRoute,
     getListingRoute,
-    addViewToListingRoute
+    addViewToListingRoute,
+    getUserListingsRoute
 ]
