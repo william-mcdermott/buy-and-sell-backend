fe1a1c37012adbe61796fa64aececc9fa4e0a3d0
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-03-19 21:59:59 -0500

    add route to update existing listing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import { createNewListingRoute } from "./createNewListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { getUserListingsRoute } from "./getUserListings";
+import { updateListingRoute } from "./updateListing";
 
 export default [
     getAllListingsRoute,
     getListingRoute,
     addViewToListingRoute,
     getUserListingsRoute,
-    createNewListingRoute
+    createNewListingRoute,
+    updateListingRoute
 ]
